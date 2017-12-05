2e63b0798c7a8c33d33800517969bba8ddbeb343
shishaoqi-ubuntu <shishaoqi@huayun.com>
2017-12-05 15:49:19 +0800

    发布视图-bug3

--- src/Providers/TotemServiceProvider.php
+++ src/Providers/TotemServiceProvider.php
@@ -106,7 +106,7 @@ class TotemServiceProvider extends ServiceProvider
         $this->loadViewsFrom(__DIR__.'/../../resources/views/vendor', 'laravel-totem');
 
         $this->publishes([
-            __DIR__.'/../../resources/views/vendor' => resource_path('views/vendor/laravel-totem'),
+            __DIR__.'/../../resources/views' => resource_path('views/vendor/laravel-totem'),
         ], 'view');
     }
 }
